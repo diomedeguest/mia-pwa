f2bfceed0d67c11ec6b9e66cd50ea46e06d26a41
diomedeguest <diomedeguest@gmail.com>
2026-08-24 23:42:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,82 @@
-const CACHE_NAME = 'diomede-v30';
+const CACHE_NAME = 'diomede-v53';
 
-const ASSETS = [
+const CORE_ASSETS = [
+  './assets/images/caffe_espresso.png',
   './',
   './index.html',
   './manifest.json',
   './logo.png',
-  './assets/css/style.css',
+  './logo_start.png',
+  './logowelcome.png',
+  './sfondo.png',
   './assets/images/header.png',
-  './assets/images/logo_start.png',
-  './assets/images/logomenu.png',
-  './assets/images/menu.png',
   './assets/images/sfondo.png',
+  './assets/images/idromassaggio-pannello-guida.png',
   './pages/menu.html',
-  './pages/istruzioni.html'
+  './pages/dettagli.html',
+  './pages/contatti.html',
+  './pages/regole.html',
+  './pages/istruzioni.html',
+  './pages/istruzioni/induzione.html',
+  './pages/istruzioni/depuratore.html',
+  './pages/istruzioni/cassaforte.html',
+  './pages/istruzioni/idromassaggio.html',
+  './pages/istruzioni/lavatrice.html',
+  './pages/istruzioni/asciugatrice.html',
+  './pages/istruzioni/armadio-servizio.html',
+  './assets/images/armadio-servizio.png',
+  './pages/wifi.html',
+  './pages/trasporti.html',
+  './pages/turismo.html',
+  './pages/convenzioni.html',
+  './pages/checkout.html',
+  './assets/css/style.css',
+  './pages/istruzioni/microonde.html',
+  './pages/istruzioni/friggitrice.html',
+  './pages/istruzioni/caffe-americano.html',
+  './pages/istruzioni/caffe-espresso.html',
+  './pages/istruzioni/ghiaccio.html'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // Caricamento individuale: se un file manca, gli altri vengono salvati comunque
-      return Promise.allSettled(ASSETS.map(url => cache.add(url)));
-    }).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
-    }).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request).then(r => r || caches.match('./pages/menu.html')))
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
   );
 });
